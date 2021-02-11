a73df612cdc2c861f7089be121f8373a35e2ed5b
fcomarcet1 <fcomarcet1@gmail.com>
2021-02-11 10:31:43 +0100

    Servidor funcionando con rutas

--- controllers/project.js
+++ controllers/project.js
@@ -1,19 +1,63 @@
 'use strict';
 
+var Project = require('../models/project');
+
 var controller =
     {
         // Methods
+
+        /**
+         * @param {any} req
+         * @param {Request<P, ResBody, ReqBody, ReqQuery, Locals>|http.ServerResponse} res
+         */
         home: function (req, res) {
             return res.status(200).send({
                 message: "Home controller from project: OK"
             });
         },
 
+        /**
+         * @param {any} req
+         * @param {Request<P, ResBody, ReqBody, ReqQuery, Locals>|http.ServerResponse} res
+         */
         test: function (req, res) {
             return res.status(200).send({
                 message: "Test controller from project: OK"
             });
-        }
+        },
+
+        // CRUD
+
+
+        saveProject: function(req, res) {
+
+            var project = new Project();
+
+            // Received the data from the body of the request
+            var params = req.body;
+
+            // Set values at object
+            project.name = params.name;
+            project.description = params.description;
+            project.category = params.category;
+            project.year = params.year;
+            project.langs = params.langs;
+            project.image = null;
+
+            project.save((err, projectStored) => {
+
+                // Check errors
+                if(err) {
+                    return res.status(500).send({message: 'Error al guardar el documento.'});
+                }
+                if(!projectStored) {
+                    return res.status(404).send({message: 'No se ha podido guardar el proyecto.'});
+                }
+
+                return res.status(200).send({project: projectStored});
+            });
+        },
+
     };
 
 
--- models/Project.js
+++ models/Project.js
@@ -1,22 +1,16 @@
-'use strict';
+'use strict'
 
-// load mongoose
 var mongoose = require('mongoose');
+var schema = mongoose.Schema;
 
-var schema = mongoose.schema;
-var ProjectSchema = schema(
-    {
-         name: String,
-         description: String,
-         category: String,
-         language: String,
-         year: Number,
-         image: String
-    });
+var ProjectSchema = schema({
+    name: String,
+    description: String,
+    category: String,
+    language: String,
+    year: Number,
+    image: String
+});
 
-// Modules exports
-/*
-* Mongoose asigna los names en minuscula y plural por eso podemos poner Project y en nuestra database
-* aparece projects
-*/
-module.exports = mongoose.model("Project", ProjectSchema);
+module.exports = mongoose.model('Project', ProjectSchema);
+// projects  --> guarda los documents en la coleccion
